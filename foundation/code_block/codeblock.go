9cc0063876ffd8b098a78856300eb14ebc55f9f9
Hasan Gursoy <hasangursoy@unplugdining.com>
2025-09-04 21:46:56 +0300

    code block width fix

--- foundation/code_block/codeblock.go
+++ foundation/code_block/codeblock.go
@@ -4,6 +4,7 @@ import (
 	gfcode "github.com/gofred-io/gofred/basic/code"
 	"github.com/gofred-io/gofred/basic/pre"
 	"github.com/gofred-io/gofred/foundation/column"
+	"github.com/gofred-io/gofred/foundation/container"
 	icondata "github.com/gofred-io/gofred/foundation/icon/icon_data"
 	iconbutton "github.com/gofred-io/gofred/foundation/icon_button"
 	"github.com/gofred-io/gofred/foundation/row"
@@ -13,31 +14,34 @@ import (
 )
 
 func New(code string) widget.BaseWidget {
-	return column.New(
-		[]widget.BaseWidget{
-			row.New(
-				[]widget.BaseWidget{
-					spacer.New(),
-					iconbutton.New(
-						icondata.ContentCopy,
-						iconbutton.Class("gf-code-block-copy-button"),
-						iconbutton.OnClick(func(this widget.BaseWidget, e widget.Event) {
-							utils.CopyToClipboard(code)
-						}),
-					),
-				},
-				row.Class("gf-code-block-header"),
-			),
-			pre.New(
-				gfcode.New(
-					code,
-					gfcode.FontSize(14),
-					gfcode.FontColor("#F3F4F6"),
-					gfcode.FontWeight("400"),
+	return container.New(
+		column.New(
+			[]widget.BaseWidget{
+				row.New(
+					[]widget.BaseWidget{
+						spacer.New(),
+						iconbutton.New(
+							icondata.ContentCopy,
+							iconbutton.Class("gf-code-block-copy-button"),
+							iconbutton.OnClick(func(this widget.BaseWidget, e widget.Event) {
+								utils.CopyToClipboard(code)
+							}),
+						),
+					},
+					row.Class("gf-code-block-header"),
 				),
-				pre.Class("gf-code-block-pre"),
-			),
-		},
-		column.Class("gf-code-block"),
+				pre.New(
+					gfcode.New(
+						code,
+						gfcode.FontSize(14),
+						gfcode.FontColor("#F3F4F6"),
+						gfcode.FontWeight("400"),
+					),
+					pre.Class("gf-code-block-pre"),
+				),
+			},
+			column.Flex(1),
+		),
+		container.Class("gf-code-block"),
 	)
 }
