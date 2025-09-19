13dc8b5509d46c680f3b81184fb0706bb514728a
Hasan Gursoy <hasangursoy@unplugdining.com>
2025-09-19 16:23:26 +0300

    codeblock copied icon

--- foundation/code_block/codeblock.go
+++ foundation/code_block/codeblock.go
@@ -1,17 +1,23 @@
 package codeblock
 
 import (
+	"time"
+
 	"github.com/gofred-io/gofred/application"
 	"github.com/gofred-io/gofred/basic/code"
 	gfcode "github.com/gofred-io/gofred/basic/code"
 	"github.com/gofred-io/gofred/basic/pre"
+	"github.com/gofred-io/gofred/breakpoint"
 	"github.com/gofred-io/gofred/foundation/column"
 	"github.com/gofred-io/gofred/foundation/container"
+	"github.com/gofred-io/gofred/foundation/icon"
 	icondata "github.com/gofred-io/gofred/foundation/icon/icon_data"
 	iconbutton "github.com/gofred-io/gofred/foundation/icon_button"
 	"github.com/gofred-io/gofred/foundation/row"
 	"github.com/gofred-io/gofred/foundation/spacer"
 	"github.com/gofred-io/gofred/hooks"
+	"github.com/gofred-io/gofred/listenable"
+	"github.com/gofred-io/gofred/options/spacing"
 	"github.com/gofred-io/gofred/theme"
 	"github.com/gofred-io/gofred/theme/theme_style"
 	"github.com/gofred-io/gofred/utils"
@@ -31,23 +37,45 @@ func New(innerCode string, opts ...Option) application.BaseWidget {
 		option(codeBlock)
 	}
 
+	copied, setCopied := hooks.UseState(false)
+
 	return container.New(
 		column.New(
 			[]application.BaseWidget{
 				row.New(
 					[]application.BaseWidget{
 						spacer.New(),
-						iconbutton.New(
-							icondata.ContentCopy,
-							iconbutton.Class("gf-code-block-copy-button"),
-							iconbutton.Tooltip("Copy to clipboard"),
-							iconbutton.OnClick(func(this application.BaseWidget, e application.Event) {
-								utils.CopyToClipboard(innerCode)
-								if codeBlock.onCopied != nil {
-									codeBlock.onCopied(innerCode)
-								}
-							}),
-						),
+						listenable.Builder(copied, func() application.BaseWidget {
+							if copied.Value() {
+								return container.New(
+									icon.New(
+										icondata.CheckAll,
+										icon.Fill("#2B799B"),
+										icon.Width(breakpoint.All(20)),
+										icon.Height(breakpoint.All(20)),
+									),
+									container.BackgroundColor("transparent"),
+									container.Padding(breakpoint.All(spacing.All(4))),
+								)
+							}
+							return iconbutton.New(
+								icondata.ContentCopy,
+								iconbutton.Class("gf-code-block-copy-button"),
+								iconbutton.Tooltip("Copy to clipboard"),
+								iconbutton.OnClick(func(this application.BaseWidget, e application.Event) {
+									utils.CopyToClipboard(innerCode)
+
+									setCopied(true)
+									time.AfterFunc(2*time.Second, func() {
+										setCopied(false)
+									})
+
+									if codeBlock.onCopied != nil {
+										codeBlock.onCopied(innerCode)
+									}
+								}),
+							)
+						}),
 					},
 					row.Class("gf-code-block-header"),
 				),
